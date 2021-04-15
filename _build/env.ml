16e5748041ab44374af7a89a4d1a1e22c3b359b1
Yilin Sun <ys512@cam.ac.uk>
2021-04-15 12:45:03 +0100

    v1

--- _build/env.ml
+++
@@ -1,3 +0,0 @@
-let tag_env : ((string * string list) list) ref = ref []
-
-let type_env : ((string * Ntype.ntype) list) ref = ref []
--- _build/util.ml
+++
@@ -1,9 +0,0 @@
-let word_size = 32
-
-let undef field name = failwith (field ^ ": " ^ name ^ "undefined")
-
-let find x env name =
-	try List.assoc x env
-	with Not_found -> undef name x
-
-let bits_to_words b = b/word_size, b mod word_size
